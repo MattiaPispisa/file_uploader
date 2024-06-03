22bfc7feeac4ba48c5820e8cdd44a1f361bf5370
Mattia Pispisa <mattia.pispisa@gmail.com>
2024-06-03 08:04:42 +0200

    more logger doc

--- file_uploader/lib/src/logger.dart
+++ file_uploader/lib/src/logger.dart
@@ -1,5 +1,27 @@
 /// an abstract logger used inside the library to report
 /// info, warning and errors
+///
+/// Concrete example:
+/// ```dart
+/// // It's just an example, not a production-ready version.
+/// // Every log is printed
+/// class PrinterLogger implements FileUploaderLogger {
+///   @override
+///   void error(String message, error, stackTrace) {
+///     print(error);
+///   }
+///
+///   @override
+///   void info(String message) {
+///     print(message);
+///   }
+///
+///   @override
+///   void warning(String message) {
+///     print(message);
+///   }
+/// }
+/// ```
 abstract class FileUploaderLogger {
   /// info [message]
   void info(String message);
